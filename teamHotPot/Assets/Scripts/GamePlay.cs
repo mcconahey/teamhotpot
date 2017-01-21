95771c4ae47dd139d56bc0f7a387748102a821a8
CelineTai <naughty8212@gmail.com>
2017-01-21 16:18:35 +0800

    Add GamePlay Scripts & Scene Update

---
+++ teamHotPot/Assets/Scripts/GamePlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour {
+
+	public GameObject Boar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boar") {
+			print (other + " is hitting , you fail!");
+		}
+
+	}
+}
